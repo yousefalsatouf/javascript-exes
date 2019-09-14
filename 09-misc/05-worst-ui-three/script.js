95c166af57c243c54a2b5d4fc7772e8b7ef50b20
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-09-14 21:45:52 +0200

    misc exe is done

--- 09-misc/05-worst-ui-three/script.js
+++ 09-misc/05-worst-ui-three/script.js
@@ -16,6 +16,7 @@
     let inputOne = document.getElementById("part-one");
     let btnOne = document.getElementById("fix-part-one");
     let output = document.getElementById("target");
+    output.innerHTML = "+";
     btnOne.innerHTML = "Start";
     let inputTwo = document.getElementById("part-two");
     let btnTwo = document.getElementById("fix-part-two");
@@ -32,24 +33,34 @@
     let min99 = inputTwo.getAttribute("data-min");
 
     let timer1, timer2, timer3, timer4;
-    let getValues = "";
-
     let click1 = 0;
+    let span1 = document.createElement("span");
+    span1.id = "span1";
+    output.append(span1);
+    let getVal1 = document.getElementById("span1");
+    span1.innerHTML = "460";
+
+
     btnOne.addEventListener("click", function() {
         click1++;
         if (click1 % 2 == 1) {
             btnOne.innerHTML = "Stop";
             timer1 = setInterval(() => {
                 if (inputOne.value == parseInt(max499)) inputOne.value = min499;
-
-                output.innerHTML = inputOne.value;
+                inputOne.value = parseInt(inputOne.value) + 1;
+                getVal1.innerHTML = inputOne.value;
             }, 300);
         } else {
             btnOne.innerHTML = "Start";
             clearInterval(timer1);
         }
-        inputOne.value = parseInt(inputOne.value) + 1;
     });
+    let span2 = document.createElement("span");
+    span2.id = "span2";
+    output.append(span2);
+    let getVal2 = document.getElementById("span2");
+    span2.innerHTML = "00";
+
     let click2 = 0;
     btnTwo.addEventListener("click", function() {
         click2++;
@@ -57,15 +68,23 @@
             btnTwo.innerHTML = "Stop";
             timer2 = setInterval(() => {
                 if (inputTwo.value == parseInt(max99)) inputTwo.value = min99;
-                if (parseInt(inputTwo.value) < 9)
+                if (parseInt(inputTwo.value) < 9) {
                     inputTwo.value = "0" + (parseInt(inputTwo.value) + 1);
-                else inputTwo.value = parseInt(inputTwo.value) + 1;
+                } else {
+                    inputTwo.value = parseInt(inputTwo.value) + 1;
+                }
+                getVal2.innerHTML = inputTwo.value;
             }, 300);
         } else {
             btnTwo.innerHTML = "Start";
             clearInterval(timer2);
         }
     });
+    let span3 = document.createElement("span");
+    span3.id = "span3";
+    output.append(span3);
+    let getVal3 = document.getElementById("span3");
+    span3.innerHTML = "00";
     let click3 = 0;
     btnThree.addEventListener("click", function() {
         click3++;
@@ -73,15 +92,23 @@
             btnThree.innerHTML = "Stop";
             timer3 = setInterval(() => {
                 if (inputThree.value == parseInt(max99)) inputThree.value = min99;
-                if (parseInt(inputThree.value) < 9)
+                if (parseInt(inputThree.value) < 9) {
                     inputThree.value = "0" + (parseInt(inputThree.value) + 1);
-                else inputThree.value = parseInt(inputThree.value) + 1;
+                } else {
+                    inputThree.value = parseInt(inputThree.value) + 1;
+                }
+                getVal3.innerHTML = inputThree.value;
             }, 300);
         } else {
             btnThree.innerHTML = "Start";
             clearInterval(timer3);
         }
     });
+    let span4 = document.createElement("span");
+    span4.id = "span4";
+    output.append(span4);
+    let getVal4 = document.getElementById("span4");
+    span4.innerHTML = "00";
     let click4 = 0;
     btnFour.addEventListener("click", function() {
         click4++;
@@ -89,23 +116,16 @@
             btnFour.innerHTML = "Stop";
             timer4 = setInterval(() => {
                 if (inputFour.value == parseInt(max99)) inputFour.value = min99;
-                if (parseInt(inputFour.value) < 9)
+                if (parseInt(inputFour.value) < 9) {
                     inputFour.value = "0" + (parseInt(inputFour.value) + 1);
-                else inputFour.value = parseInt(inputFour.value) + 1;
+                } else {
+                    inputFour.value = parseInt(inputFour.value) + 1;
+                }
+                getVal4.innerHTML = inputFour.value;
             }, 300);
         } else {
             btnFour.innerHTML = "Start";
             clearInterval(timer4);
         }
     });
-    let partOne = inputOne.value;
-    let partTwo = inputTwo.value;
-    let partThree = inputThree.value;
-    let partFour = inputFour.value;
-
-    if (partOne <= 9 || partFour <= 9 || partThree <= 9 || partFour <= 9)
-        getValues =
-        "+" + partOne + "0" + partTwo + "0" + partThree + "0" + partFour;
-    else getValues = "+" + partOne + partTwo + partThree + partFour;
-    output.innerHTML = getValues;
 })();
