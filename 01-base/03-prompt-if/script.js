8959d327dd5141c3a6892032f30492f58097dddb
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-09-14 20:28:10 +0200

    base exe is done

---
+++ 01-base/03-prompt-if/script.js
@@ -0,0 +1,22 @@
+/* becode/javascript
+ *
+ * /01-base/03-prompt-if/script.js - 1.3: prompt & if
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+})();
+
+let question = prompt('Would you like some Cake? oui/non');
+
+if (question == "oui") {
+    alert('Goood choice!');
+} else {
+    alert('Dude!, it is a cake!');
+}
---
+++ 01-base/04-asv-confirm/script.js
@@ -0,0 +1,30 @@
+/* becode/javascript
+ *
+ * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+})();
+
+let question1, question2, question3, confirm;
+
+do {
+    question1 = prompt('Enter Your Age!');
+    question2 = prompt('Enter Your sex!');
+    question3 = prompt('Enter Your city!');
+
+    alert('So your info is ' + '\n Age : ' + question1 +
+        '\n Sex : ' + question2 +
+        '\n City : ' + question3);
+    confirm = prompt('Are you confirming your info? yes/no');
+    if (confirm == "yes") {
+        alert('Done');
+    }
+} while (confirm == "no")
